70b93d12d8c316ab420b4373ecff85f46adfda56
mountain-kangkang <rngl10@naver.com>
2024-11-29 11:21:44 +0900

    base django setting
    장고 프로젝트 다시 시작

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = "config.urls.urls_dev"
 
 TEMPLATES = [
     {
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dj_bank",
+        "USER": "root",
+        "PASSWORD": "0000",
+        "HOST": "localhost",
     }
 }
 
