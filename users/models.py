d73924753d21e9d975e5199aef685c34ecdeb29e
mountain-kangkang <rngl10@naver.com>
2024-12-02 00:44:12 +0900

    build : users model 생성
    -users model
    -- email
    -- password(기본생성)
    -- nickname
    -- username(기본생성)
    -- phone_num
    -- last_login(기본생성)
    -- is_staff(기본생성)
    -- is_superuser(기본생성)
    -- is_active(기본생성)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=45 , unique=True, null=False)
+    nickname = models.CharField(max_length=15, unique=True, null=False)
+    phone_num = models.CharField(max_length=15, unique=True, null=False)
