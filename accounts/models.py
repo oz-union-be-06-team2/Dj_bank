65cc69116c009ea10332d686352f918e3ba064e1
Kyubin1123 <michelle010929@gmail.com>
2024-12-02 13:55:14 +0900

    chore : model.py 수정
    - model.py 수정
    -- accounts id(자동생성으로 삭제)
    -- transactions id(자동생성으로 삭제)
    -- transactions-account fk ('accounts.id' > accounts)

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,16 @@
-import uuid
-
 from django.db import models
 
 class accounts(models.Model):
-    accounts_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     account_num = models.IntegerField()
     bank_code = models.IntegerField()
-    transaction_type = models.CharField(max_length=45)
+    account_type = models.CharField(max_length=45)
     balance = models.IntegerField()
-    user_id = models.ForeignKey('users.id', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.id', on_delete=models.CASCADE)
 
 class transactions(models.Model):
-    transaction_id = models.BigIntegerField(primary_key=True)
-    after_balance = models.IntegerField()
+    balance = models.IntegerField()
     transaction_detail = models.CharField(max_length=45)
     transaction_type = models.CharField(max_length=45)
     transaction_method = models.CharField(max_length=45)
     transaction_time = models.DateTimeField()
-    account_num = models.ForeignKey('accounts.accounts_id', on_delete=models.CASCADE)
+    account = models.ForeignKey(accounts, on_delete=models.CASCADE)
