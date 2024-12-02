74be5ad280431006daa7311aefa1ef161316ea57
Kyubin1123 <michelle010929@gmail.com>
2024-12-02 11:17:11 +0900

    build : accounts model 생성
    - accounts model.py 작성(accounts, transactions)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class accounts(models.Model):
+    accounts_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    account_num = models.IntegerField()
+    bank_code = models.IntegerField()
+    transaction_type = models.CharField(max_length=45)
+    balance = models.IntegerField()
+    user_id = models.ForeignKey('users.id', on_delete=models.CASCADE)
+
+class transactions(models.Model):
+    transaction_id = models.BigIntegerField(primary_key=True)
+    after_balance = models.IntegerField()
+    transaction_detail = models.CharField(max_length=45)
+    transaction_type = models.CharField(max_length=45)
+    transaction_method = models.CharField(max_length=45)
+    transaction_time = models.DateTimeField()
+    account_num = models.ForeignKey('accounts.accounts_id', on_delete=models.CASCADE)
